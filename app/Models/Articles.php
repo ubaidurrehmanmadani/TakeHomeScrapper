b6f4e18994d9bad88d28cd4dc45199768493413f
Ubaid Ur Rehman <ubaidurrehmanmadani@gmail.com>
2023-12-04 05:03:30 +0500

    master

--- app/Models/Articles.php
+++ app/Models/Articles.php
@@ -22,23 +22,27 @@ class Articles extends Model
         $query = $author ? $query->where('author', 'like', '%' . $author . '%') : $query;
         $query = $publishedAt ? $query->whereDate('published_at', '=', $publishedAt) : $query;
 
-        $result = collect($query->get())->map(function ($item) use ($category) {
-            if ($item->api_source_id == 1) {
-                if (str_contains($item->title, $category)) {
-                    return $item;
-                }
+        if($category){
+            $result = collect($query->get())->map(function ($item) use ($category) {
+                if ($item->api_source_id == 1) {
+                    if (str_contains($item->title, $category)) {
+                        return $item;
+                    }
 
-            } else if ($item->api_source_id == 2 && !empty($item->categories)) {
-                $itemCategories = json_decode($item->categories, true);
-                $key = in_array($category, $itemCategories, true);
-                if ($key !== false) {
-                    return $item;
+                } else if ($item->api_source_id == 2 && !empty($item->categories)) {
+                    $itemCategories = json_decode($item->categories, true);
+                    $key = in_array($category, $itemCategories, true);
+                    if ($key !== false) {
+                        return $item;
+                    }
                 }
-            }
-        });
+            });
 
-       return array_filter($result->toArray(), function ($value) {
-            return $value !== null;
-        });
+            return array_filter($result->toArray(), function ($value) {
+                return $value !== null;
+            });
+        }
+
+        return $query->get();
     }
 }
